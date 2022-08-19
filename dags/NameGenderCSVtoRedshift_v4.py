459664a8a09d5d41163a106e7addd7ded8ec7933
Keeyong (Max) <keeyonghan@hotmail.com>
2022-08-19 14:56:52 -0700

    Add files via upload

---
+++ dags/NameGenderCSVtoRedshift_v4.py
@@ -0,0 +1,97 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.models import Variable
+from airflow.hooks.postgres_hook import PostgresHook
+
+from datetime import datetime
+from datetime import timedelta
+# from plugins import slack
+
+import requests
+import logging
+import psycopg2
+
+
+
+def get_Redshift_connection(autocommit=False):
+    hook = PostgresHook(postgres_conn_id='redshift_dev_db')
+    conn = hook.get_conn()
+    conn.autocommit = autocommit
+    return conn.cursor()
+
+
+def extract(**context):
+    link = context["params"]["url"]
+    task_instance = context['task_instance']
+    execution_date = context['execution_date']
+
+    logging.info(execution_date)
+    f = requests.get(link)
+    return (f.text)
+
+
+def transform(**context):
+    text = context["task_instance"].xcom_pull(key="return_value", task_ids="extract")
+    lines = text.split("\n")[1:]
+    return lines
+
+
+def load(**context):
+    schema = context["params"]["schema"]
+    table = context["params"]["table"]
+    
+    cur = get_Redshift_connection()
+    lines = context["task_instance"].xcom_pull(key="return_value", task_ids="transform")
+    sql = "BEGIN; DELETE FROM {schema}.{table};".format(schema=schema, table=table)
+    for line in lines:
+        if line != "":
+            (name, gender) = line.split(",")
+            print(name, "-", gender)
+            sql += f"""INSERT INTO {schema}.{table} VALUES ('{name}', '{gender}');"""
+    sql += "END;"
+    logging.info(sql)
+    cur.execute(sql)
+
+
+dag_second_assignment = DAG(
+    dag_id = 'second_assignment_v4',
+    start_date = datetime(2022,5,12), # 날짜가 미래인 경우 실행이 안됨
+    schedule_interval = '0 2 * * *',  # 적당히 조절
+    max_active_runs = 1,
+    catchup = False,
+    default_args = {
+        'retries': 1,
+        'retry_delay': timedelta(minutes=3),
+        # 'on_failure_callback': slack.on_failure_callback,
+    }
+)
+
+
+extract = PythonOperator(
+    task_id = 'extract',
+    python_callable = extract,
+    params = {
+        'url':  Variable.get("csv_url")
+    },
+    provide_context=True,
+    dag = dag_second_assignment)
+
+transform = PythonOperator(
+    task_id = 'transform',
+    python_callable = transform,
+    params = { 
+    },  
+    provide_context=True,
+    dag = dag_second_assignment)
+
+load = PythonOperator(
+    task_id = 'load',
+    python_callable = load,
+    params = {
+        'schema': 'keeyong',   ## 자신의 스키마로 변경
+        'table': 'name_gender'
+    },
+    provide_context=True,
+    dag = dag_second_assignment)
+
+extract >> transform >> load
