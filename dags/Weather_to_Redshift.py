e779a16fe22a12f7c30faa5ac06ed6a5d107f8c9
Keeyong (Max) <keeyonghan@hotmail.com>
2022-08-26 14:10:42 -0700

    Update Weather_to_Redshift.py

--- dags/Weather_to_Redshift.py
+++ dags/Weather_to_Redshift.py
@@ -59,7 +59,7 @@ CREATE TABLE keeyong.weather_forecast (
 
 dag = DAG(
     dag_id = 'Weather_to_Redshift',
-    start_date = datetime(2021,12,10), # 날짜가 미래인 경우 실행이 안됨
+    start_date = datetime(2022,8,24), # 날짜가 미래인 경우 실행이 안됨
     schedule_interval = '0 2 * * *',  # 적당히 조절
     max_active_runs = 1,
     catchup = False,
