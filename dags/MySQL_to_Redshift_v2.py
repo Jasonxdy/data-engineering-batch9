c2e1408d06fb263d1243bc88aed4db5a664a7f50
Keeyong (Max) <keeyonghan@hotmail.com>
2022-08-26 14:11:48 -0700

    Update MySQL_to_Redshift_v2.py

--- dags/MySQL_to_Redshift_v2.py
+++ dags/MySQL_to_Redshift_v2.py
@@ -16,7 +16,7 @@ import json
 
 dag = DAG(
     dag_id = 'MySQL_to_Redshift_v2',
-    start_date = datetime(2021,11,27), # 날짜가 미래인 경우 실행이 안됨
+    start_date = datetime(2022,8,24), # 날짜가 미래인 경우 실행이 안됨
     schedule_interval = '0 9 * * *',  # 적당히 조절
     max_active_runs = 1,
     catchup = True,
